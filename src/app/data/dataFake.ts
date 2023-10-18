dab415564878bc6cf4c4bfaa0aab3947537adaa3
lucascbarone <lucascbarone@hotmail.com>
2023-10-18 03:21:13 -0300

    first commit

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,14 +1,26 @@
 export const dataFake = [
   {
     "id":"1",
-    "title": "NOVO HOMEM DE FERRO EM 3D",
-    "description": "marvel anuncia um novo filme do homem de ferro, confira",
-    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F51FA9F6CBD9F0C9B1394B1CC0A6A842D07091318674E234CD33CBF7C28CDC3/scale?width=1200&aspectRatio=1.78&format=jpeg"
+    "title": "Santander Bootcamp 2023 - Fullstack Java+Angular",
+    "description": "Atualmente, estou realizando novo bootcamp oferecido pelo Santander em parceria com a DIO. A grade educacional inclui desde os conceitos iniciais até tópicos avançados em desenvolvimento web, JavaScript, Angular, Java, bancos de dados e APIs REST, visando aumentar sua produtividade na criação de aplicativos web modernos e escaláveis.",
+    "photoCover":"https://hermes.dio.me/tracks/afebe5ed-2b18-438a-95b0-2c971e9aeff9.png"
   },
   {
     "id":"2",
-    "title": "Nova Série anunciada no Disney +",
-    "description": "bla blabla",
-    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+    "title": "Potência Tech powered by iFood | Ciência de Dados",
+    "description": "Último bootcamp completado pela DIO. Este curso foi uma parceria entre iFood e DIO, a qual trouxe os princípios da Ciência de Dados, utilizando Python e SQL, por meio de cursos, desafios de projeto e desafios de código. Além disso, exploramos temas fundamentais, como Modelagem de Dados e conceitos básicos de Aprendizado de Máquina.",
+    "photoCover":"https://hermes.dio.me/tracks/f5dba255-da18-427a-a02a-ca11a339c1cd.png"
+  },
+  {
+    "id":"3",
+    "title": "Fundação Bradesco - Trilha de Conhecimento | Linguagem de Programação Python",
+    "description": "Trilha de Conhecimento completada pela Fundação Bradesco. Nesta trilha, foram apresentadas as principais bibliotecas de código aberto e os principais bancos de dados, os quais tornaram-se um requisito fundamental para analistas de Dados e desenvolvedores. Além disso, foi apresentado a linguagem de consulta SQL e a importância do modelo Entidade Relacionamento na estrutura de um banco de dados. Por fim, foi feita a importação de um database para o SQLite Studio via interface gráfica, na qual foram criadas e manipuladas tabelas por meio de Queries.",
+    "photoCover":"https://agazetaempregosmg.com.br/images/fundacao_bradesco.jpg"
+  },
+  {
+    "id":"4",
+    "title": "Instituto Federal Rio Grande do Sul | Certificado de Banco de Dados 1: fundamentos",
+    "description": "Curso de fundamentos de Banco de Dados completada na plataforma Apranda Mais do MEC, em parceria com o Instituto Federal Rio Grande do Sul. Neste curso, abordou-se os conceitos básicos de banco de dados, incluindo a modelagem relacional e normalização. Conceitos fundamentais para que se consiga estudar e desenvolver programas que utilizem a linguagem SQL.",
+    "photoCover":"https://aprendamais.mec.gov.br/pluginfile.php/389787/course/overviewfiles/database-storage.png"
   }
 ]
